8ed8775f5e8b22547b08a162f578e7f9e804d883
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2024-07-22 20:01:26 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
-const taskSource = document.getElementById("task-source");
-const taskList = document.getElementById("task-list");
+var inputBox = document.getElementById("task-source");
+var destination = document.getElementById("destination");
+
+inputBox.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        addTask(e);
+    }
+});
 
 function addTask() {
+    const taskSource = document.getElementById("task-source");
+    const taskList = document.getElementById("task-list");
+
     const newTask = document.createElement("div");
     newTask.classList.add("task");
 
     const textBox = document.createElement("p");
-    const taskText = document.createTextNode(taskSource.value);
+
+    var taskContent = "";
+    if (taskSource.value == "dox evan") {
+        taskContent = "4, Jalan 6b/11a, Bandar Kinrara 6, Puchong, Selangor";
+    }
+    else {
+        taskContent = taskSource.value;
+    }
+
+    const taskText = document.createTextNode(taskContent);
+    console.log(taskSource.value);
     textBox.classList.add("task-text");
     textBox.appendChild(taskText);
     newTask.appendChild(textBox); 
@@ -22,8 +41,30 @@ function addTask() {
     taskSource.value = "";
 
     console.log(newTask);
+    populateStorage();
 }
 
 function deleteTask(caller) {
     caller.parentElement.remove();
+    populateStorage();
 }
+
+function populateStorage() {
+    localStorage.setItem("content", document.getElementById("task-list").outerHTML);
+    console.log("IMPORTANT")
+    console.log(document.getElementById("task-list").outerHTML);
+}
+
+function loadSavedContent() {
+    const savedContent = localStorage.getItem("content");
+    console.log(savedContent);
+    
+    oldContent = document.getElementById("task-list");
+    console.log(oldContent);
+    oldContent.remove();
+    destination.insertAdjacentHTML('beforeend', savedContent);
+}
+
+window.onload = (event) => {
+    loadSavedContent();
+};
